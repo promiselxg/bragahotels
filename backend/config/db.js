08b2d91f9c5423ce994cfcc7e3abe3cea8ecc87c
promiselxg <okeydeede@gmail.com>
2022-04-08 16:27:13 +0100

    added authController

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,13 +7,13 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    operatorsAliases: false,
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 1000,
     },
+    logging: false,
   }
 );
 
@@ -34,8 +34,6 @@ db.sequelize = sequelize;
 // create Table
 db.users = require('../model/userModel')(sequelize, DataTypes);
 
-db.sequelize.sync({ force: false }).then(() => {
-  console.log('yes re-sync done!');
-});
+db.sequelize.sync({ force: false }).then(() => {});
 
 module.exports = db;
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,16 @@
 const asyncHandler = require('express-async-handler');
+const db = require('../config/db');
 
 //@desc     Register User
 //@route    GET /api/auth
 //@access   Public
 const registerUser = asyncHandler(async (req, res) => {
+  const newUser = await db.users.create({
+    title: req.body.name,
+  });
   res.status(200).json({
     status: true,
-    message: 'register route',
+    message: newUser,
   });
 });
 
